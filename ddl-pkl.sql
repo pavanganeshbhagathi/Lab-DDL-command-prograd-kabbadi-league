ddd84ec22a44787e54a033b056ac444f2b7e7e91
pavanganeshbhagathi <pavanganeshbhagathi554@gmail.com>
2020-05-27 09:56:09 +0530

    did

--- ddl-pkl.sql
+++ ddl-pkl.sql
@@ -1,117 +1,134 @@
 -- PROGRESSION - 1
 
 
--- 1.Create table city
-
-CREATE TABLE city(
-id NUMBER(11),
-name VARCHAR(50)
-);
-
-
-
+-- 1. **Create table city**
+create table city(id int not null AUTO_INCREMENT,name varchar(50),primary key(id));
 -- 2. **Create table referee**
-
-CREATE TABLE referee(
-id NUMBER(11),
-name VARCHAR(50)
-);
-
-
+create table referee(referee_id  int not null auto_increment,referee_name varchar(100),primary key(referee_id));
 -- 3. **Create table innings**
-
-CREATE TABLE innings(
-id NUMBER(11),
-innings_number NUMBER(11)
-);
-
+create table innings(innings_id  int not null auto_increment,innings_number int,primary key(innings_id));
 -- 4. **Create table extra_type**
-CREATE TABLE extra_type(
-id NUMBER(11),
-name VARCHAR(50)
-
-);
+create table extra_type(extra_type_id  int not null auto_increment,extra_type_name varchar(50),primary key(extra_type_id));
 -- 5. **Create table skill**
-CREATE TABLE skill(
-id NUMBER(11),
-name VARCHAR(50)
-);
+CREATE TABLE skill(skill_id bigint not null auto_increment,skill_name VARCHAR(50),primary key(skill_id));
 -- 6. **Create table team**
-CREATE TABLE team(
-id NUMBER(11),
-name VARCHAR(50),
-coach VARCHAR(50),
-home_city NUMBER(11),
-captain NUMBER(11)
-);
-
+   create table team (
+    id  int auto_increment not null,
+    name varchar(120),
+	coach varchar(120),
+    home_city int not null,
+	captain int,
+    foreign key (home_city) references city(city_id),
+    primary key (id)
+  ) ENGINE = InnoDB;
 -- 7. **Create table player**
-CREATE TABLE player(
-id NUMBER(11),
-name VARCHAR(50),
-country VARCHAR(50),
-skill_id NUMBER(11),
-team_id NUMBER(11)
-);
+
+ create table player (
+    player_id  int auto_increment not null,
+    player_name varchar(50),
+	player_country varchar(120),
+    skill_id1 bigint not null ,
+	team_id int not null,
+    foreign key (team_id) references team(id),
+	foreign key (skill_id1) references skill(skill_id),
+    primary key (player_id)
+  ) ENGINE = InnoDB;
+
 
 -- 8. **Create table venue**
-CREATE TABLE venue(
-id NUMBER(11),
-stadium_name VARCHAR(50),
-city_id NUMBER(11)
-);
+ create table venue (
+    venue_id  int auto_increment not null,
+    stadium_name varchar(50),
+	city_id1 int not null,
+	foreign key (city_id1) references city(id),
+	primary key (venue_id)
+  ) ENGINE = InnoDB;
 
 -- 9. **Create table event**
 CREATE TABLE event(
-id NUMBER(11),
-innings_id NUMBER(11),
-events_no NUMBER(11),
-raide_id NUMBER(11),
-raider_points NUMBER(11),
-defending_points NUMBER(11),
-clock_in_seconds NUMBER(11),
-team_one_score NUMBER(11),
-team_two_score NUMBER(11)
+event_id int not null auto_increment,
+innings_id int not null,
+events_no int,
+raide_id int not null,
+raider_points int,
+defending_points int,
+clock_in_seconds int,
+team_one_score int,
+team_two_score int,
+foreign key (innings_id) references innings(innings_id),
+foreign key (raide_id) references player(player_id),
+primary key(event_id)
 );
 
 -- 10. **Create table extra_event**
 CREATE TABLE extra_event(
-id NUMBER(11),
-event_id NUMBER(11),
-extra_type_id NUMBER(11),
-points NUMBER(11),
-scoring_team_id NUMBER(11)
+extra_event_id int not null auto_increment,
+event_id int not null,
+extra_type_id int not null,
+points int,
+scoring_team_id int not null,
+foreign key (event_id) references team(id),
+foreign key (extra_type_id) references extra_type(extra_type_id),
+foreign key (scoring_team_id) references event(event_id),
+primary key(extra_event_id)
 );
-
 -- 11. **Create table outcome**
 CREATE TABLE outcome(
-id NUMBER(11),
-status VARCHAR(100),
-winner_team_id NUMBER(11),
-score NUMBER(11),
-player_of_match NUMBER(11)
+outcome_id int not null auto_increment,
+outcome_status VARCHAR(100),
+winner_team_id int not null,
+score int,
+player_of_match int not null,
+foreign key (winner_team_id) references team(id),
+foreign key (player_of_match) references player(player_id),
+primary key (outcome_id)
 );
 -- 12. **Create table game**
 CREATE TABLE game(
-id NUMBER(11),
+game_id int,
 game_date DATE,
-team_id_1 NUMBER(11),
-team_id_2 NUMBER(11),
-venue_id NUMBER(11),
-outcome_id NUMBER(11),
-referee_id_1 NUMBER(11),
-referee_id_2 NUMBER(11),
-first_innings_id NUMBER(11),
-second_innings_id NUMBER(11)
+team_id_1 int not null,
+team_id_2 int not null,
+venue_id int not null,
+outcome_id int not null,
+referee_id_1 int not null,
+referee_id_2 int not null,
+first_innings_id int not null,
+second_innings_id int not null,
+foreign key (team_id_1) references team(id),
+foreign key (team_id_2) references team(id),
+foreign key (venue_id) references venue(venue_id),
+foreign key (outcome_id) references outcome(outcome_id),
+foreign key (referee_id_1) references referee(referee_id),
+foreign key (referee_id_2) references referee(referee_id),
+foreign key (first_innings_id) references innings(innings_id),
+foreign key (second_innings_id ) references innings(innings_id),
+primary key (game_id)
 );
+
 -- 13. **Drop table city**
-DROP TABLE city;
+ALTER TABLE team
+	DROP FOREIGN KEY team_ibfk_1;
+ALTER TABLE team 
+    DROP COLUMN home_city  //not needed just checking column deleted or not
+ALTER TABLE venue
+	DROP FOREIGN KEY venue_ibfk_1;
+drop table city;
 
 -- 14. **Drop table innings**
-DROP TABLE innings;
-
+ALTER TABLE event
+	DROP FOREIGN KEY event_ibfk_1;
+ALTER TABLE game
+	DROP FOREIGN KEY game_ibfk_7;
+ALTER TABLE game
+	DROP FOREIGN KEY game_ibfk_8;  /these all are  Constaright im not decelre in above code please do that
+drop table innings;
 -- 15. **Drop table skill**
-DROP TABLE skill;
+ALTER TABLE player
+	DROP FOREIGN KEY player_ibfk_2;
+	drop table skill
 
 -- 16. **Drop table extra_type**
-DROP TABLE extra_type;
+ALTER TABLE extra_event
+	DROP FOREIGN KEY extra_event_ibfk_2; 
+	drop table extra_type;
